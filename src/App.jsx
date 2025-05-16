dcf1602c47e39d5829d1826b87f238a814d5342d
Fred Brunner <frdbrunner@gmail.com>
2025-05-16 08:31:52 +0200

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './pages/Home';
 import './index.css'; // Ensure global styles are imported
 
 function App() {
   return (
-    <Home />
+    <>
+      <Home />
+      <Analytics />
+    </>
   );
 }
 
